f58a7780c9a567d90653895e058933d201850f97
14th-August <cs.cfadams@gmail.com>
2026-01-08 00:02:51 -0800

    added more to description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,26 @@
 export function HomeScreen() {
     return (
         <div classname="Homescreen">
-            <h3>About Me</h3>
-            <br />
-            <p>My name is Casey Adams, I am an aspiring Software Developer from Nanaimo, BC, Canada.
-                Some of my hobbys include studying Japanese, creating applicable software for my dails needs, ands cooking!
-                If you are someone who
-                
-            </p>
+            <h3>Casey Adams</h3>
+            <h5>Full Stack Software Engineer</h5>
+            <h6>I create scalable, creative, and modular 
+                applications. 
+            </h6>
+
+            <h2>I am a developer from Nanaimo BC, Canada. I am currently in 
+                enrolled at Vancouver Island University and my passion in development is 
+                in building applications for my community and colleagues. My favorite pass
+                times are studying Japanese, Cooking, Working out, and bugging my friends.
+
+                Building with the idea of scalability and practical design are what
+                drives me improve my skills as a developer. I push myself to grow
+                my technical stack and be adaptable and flexible.
+
+                Currently, I am looking for work opportunities and have been steadily honing my skills as a developer through personal projects and research.
+                If you would like to see some of my contributions as a developer. Feel free to check
+                out the project section of my website or checkout my GitHub page. If you would like
+                to contact me, then consider sending me an email by clicking the mail icon.  
+            </h2> 
         </div>
     );
 }
