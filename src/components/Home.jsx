8f885735804985bb377f2a83964972fb7acb5c35
14th-August <cs.cfadams@gmail.com>
2026-01-07 23:45:59 -0800

    added an about description

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+export function HomeScreen() {
+    return (
+        <div classname="Homescreen">
+            <h3>About Me</h3>
+            <br />
+            <p>My name is Casey Adams, I am an aspiring Software Developer from Nanaimo, BC, Canada.
+                Some of my hobbys include studying Japanese, creating applicable software for my dails needs, ands cooking!
+                If you are someone who
+                
+            </p>
+        </div>
+    );
+}
